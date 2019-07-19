fe6ad9b8eee0fcc471c5ddf94642ad13706fda84
menggithub123456 <401824065@qq.com>
2019-07-19 09:19:45 +0800

    新增添加删除

--- dubbo_interface/src/main/java/com/dubbo/service/DistrictService.java
+++ dubbo_interface/src/main/java/com/dubbo/service/DistrictService.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface DistrictService {
     public List<District> getAllDistrict();
+    public int addDistrict(District district);
+    public int delDistrict(Integer id);
 }
