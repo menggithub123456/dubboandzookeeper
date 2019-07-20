07e38cf074c94f8a4be86e65fe59a01d7784633c
menggithub123456 <401824065@qq.com>
2019-07-20 09:19:02 +0800

    新增实现jsp页面

---
+++ consumer/src/main/java/com/dubbo/consumer/globalconfig/GlobalConfig.java
@@ -0,0 +1,27 @@
+package com.dubbo.consumer.globalconfig;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class GlobalConfig {
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+        return (factory) -> {
+            factory.addContextCustomizers((context) -> {
+                        //模块中webapp相对路径
+                        String relativePath = "consumer/src/main/webapp";
+                        File docBaseFile = new File(relativePath);
+                        // 路径是否存在
+                        if (docBaseFile.exists()) {
+                            context.setDocBase(docBaseFile.getAbsolutePath());
+                        }
+                    }
+            );
+        };
+    }
+}
